4a88dba0e287bb62a00ff2d8491495bb9c69b7e7
usaid <usaidnazir@gmail.com>
2025-11-28 09:33:26 +0500

    utils for dataset utils

---
+++ oxford_flower/utils.py
@@ -0,0 +1,52 @@
+
+import scipy.io
+from torch.utils.data import DataLoader, Dataset, random_split
+import scipy
+from PIL import Image
+import os
+class OxfordDataset(Dataset):
+    def __init__(self, rootdir, transforms):
+        self.rootdir = rootdir
+        self.img_path = os.path.join(self.rootdir,"jpg")
+        self.labels_path = os.path.join(self.rootdir,"imagelabels.mat")
+        self.labels = scipy.io.loadmat(self.labels_path)
+
+        self.labels = self.labels["labels"][0] - 1
+
+        self.transforms = transforms
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self,idx):
+        try:
+            img_name = f'image_{idx + 1:05d}.jpg'
+
+            img_path = os.path.join(self.img_dir, img_name)
+            image = Image.open(img_path)
+            image.verify()
+            image = Image.open(img_path)
+            if image.size[0] < 32 or image.size[1] <32:
+                raise ValueError(f"Image too small {image.size}")
+            
+            if image.mode != "RGB":
+                image = image.convert('RGB')
+            
+            image = self.transform(image)
+            label = self.labels[idx]
+            
+        
+        
+
+            return image, label
+        except Exception as e:
+            self.error_logs.append({
+                'index':idx,
+                'error':e,
+                'path': img_path if 'img_path' in locals() else 'unknown'})
+            next_idx = (idx+1) % len(self)
+            return self.__getitem__(next_idx)
+
+        
+
+        
